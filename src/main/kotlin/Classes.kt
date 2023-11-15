d8bd6918a99f3a675e92fa54f5f9138b43a77dc6
hamza <hamzaasif219@gmail.com>
2023-11-15 18:05:12 +0500

    functions

---
+++ src/main/kotlin/Classes.kt
@@ -0,0 +1,65 @@
+fun main(){
+
+    val carolla=Car("CarollaX", "Petrol", 30)
+    val honda=Car("city","Petrol", 25)
+    carolla.InnerNestedclass().printname()
+ val nested=Car.nestedClass()// we can also use for direct // val nested=Car.nestedClass().printname()
+val prsn1=Person("h",25)
+    println(carolla.name)
+    println(carolla.type)
+    println(carolla.Km)
+    println(honda.name)
+    println(honda.type)
+    println(honda.Km)
+    honda.brakeApply()
+    carolla.driveCar()
+    println("Nested class: ${nested.printname()}")
+    println("Nested another method : "+Car.nestedClass().printname())
+    println("Inner Nested Class: "+ carolla.InnerNestedclass().printname())
+    println(carolla.name3)
+
+
+    println("Person Class ")
+    println(prsn1)
+    println("Age"+prsn1.age1)
+    println("Name :"+prsn1.name1)
+}
+
+class Car(val name:Any,val tyers:Int , val type:Any, val Km:Int)//properties
+{ // TODO: Parent Class
+    init {
+        println("$name is created")
+        println("typer is : $type ")
+    }
+    constructor(name: String, type: Any, Km: Int):this(name,5,type,Km)
+   private val name1:String="Call me"
+    private val name2:String="Private member"
+    val name3:String="Public member"
+
+    fun driveCar(){
+        println(" $name Car is driving")
+    }
+    fun brakeApply(){
+        println("$name Applied brake")
+    }
+    class nestedClass{ // TODO: Nested Class/ Child Class
+        fun printname():String{
+            return "Hello Nested class"
+
+        }
+
+    }
+   inner class InnerNestedclass{ // TODO: Inner Nested Class
+      // private val b:String="Hello inner private variable"
+       fun printname():String{
+           val a:String="hello innerNested class body"
+           println(a)
+           return name2
+       }
+
+    }
+}
+class Person(name:String, age:Int){
+    var name1=name
+    var age1=age
+}
--- src/main/kotlin/lamda.kt
+++ src/main/kotlin/lamda.kt
@@ -1,13 +1,27 @@
 fun main(){
-println(sum(2.0,6.0))
-    println(power(3.0,7.0))
-    var fn:(a:Double,b:Double)->Double=::sum
-    println(fn(2.0,4.0 ))
+//println(sum(2.0,6.0))
+//    println(power(3.0,7.0))
+//    var fn:(a:Double,b:Double)->Double=::sum
+//    println(fn(2.0,4.0 ))
+    calculator(3.2,5.6,::sum )
+    var Lambda={a:Int,b:Int-> a+b} //lambda expression
+    println(Lambda(22,4))
+
+var lambda:(Int, Int)->Int ={a,b-> a+b}// lambda expression
+println(anonymous(45.5,5.7))// anonymous function
 }
+
+
+var anonymous=fun(x:Double,y:Double):Double{
+    return x+y
+}//Anonymous function
 fun sum(a:Double,b:Double):Double{
     return a+b
 }
-//fun calculator(a:Double,b:Double)
+fun calculator(a:Double,b:Double, gn:(Double,Double)->Double){
+    val result:Double=gn(a,b)
+println(result)
+}
 fun power(a:Double,b:Double):Double{return a+b}
 fun hello(a:Int):Unit{
 
